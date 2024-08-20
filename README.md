0c793b057144a2494d5ac26a9dcc16e78ab40b69
“Foxshane997” <Foxshane997@gmail.com>
2024-08-19 21:19:32 -0700

    all working as intended

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PPP-3-Intro-Functional-Programming
+# Practice with Functional Programming (ppp-3-introduction-to-functional-programming-with-python)
+
+## https://digitalskills.instructure.com/courses/13320/pages/practice-with-functional-programming-ppp-3-introduction-to-functional-programming-with-python?module_item_id=1909911 
--- index.py
+++ index.py
@@ -1,26 +1,52 @@
-'''
-Problem 1: Write a function flatten_dict to flatten a nested dictionary by joining the keys with . character.
+# Problem 1: Write a function flatten_dict to flatten a nested dictionary by joining the keys with . character.
 
->>> flatten_dict({'a': 1, 'b': {'x': 2, 'y': 3}, 'c': 4})
-{'a': 1, 'b.x': 2, 'b.y': 3, 'c': 4}
-'''
+def flatten_dict(d):
+    flat_dict = {}
+    for key, value in d.items():
+        if isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                flat_dict[f"{key}.{sub_key}"] = sub_value
+        else:
+            flat_dict[key] = value
+    return flat_dict
 
-
-
-'''
-Problem 2: Write a function unflatten_dict to do reverse of flatten_dict.
-
->>> unflatten_dict({'a': 1, 'b.x': 2, 'b.y': 3, 'c': 4})
-{'a': 1, 'b': {'x': 2, 'y': 3}, 'c': 4}
-'''
-
-
-
-'''
-Problem 3: Write a function treemap to map a function over nested list.
-
->>> treemap(lambda x: x*x, [1, 2, [3, 4, [5]]])
-[1, 4, [9, 16, [25]]]
-'''
+nested_dict = {'a':1, 'b': {'i': 2, 'j':3}, 'c': 4}
+flattened = flatten_dict(nested_dict)
+print(flattened)
+# Expected output: {'a': 1, 'b.i': 2, 'b.j': 3, 'c': 4}
 
 
+# Problem 2: Write a function unflatten_dict to do reverse of flatten_dict.
+def unflatten_dict(d):
+    unflattened = {}
+    
+    for key, value in d.items():
+        keys = key.split('.')
+        temp_dict = unflattened
+        
+        # Navigate through the keys to find or create the nested structure
+        for part in keys[:-1]:
+            if part not in temp_dict:
+                temp_dict[part] = {}
+            temp_dict = temp_dict[part]
+        
+        # Set the value at the final key
+        temp_dict[keys[-1]] = value
+    
+    return unflattened
+
+# Example usage
+flattened_dict = {'a': 1, 'b.i': 2, 'b.j': 3, 'c': 4}
+unflattened = unflatten_dict(flattened_dict)
+print(unflattened)  # Output: {'a': 1, 'b': {'i': 2, 'j': 3}, 'c': 4}
+
+
+# Problem 3: Write a function treemap to map a function over nested list.
+def treemap(func, lst):
+    if isinstance(lst, list):
+        return [treemap(func, item) for item in lst]
+    else:
+        return func(lst)
+
+result = treemap(lambda x: x * x, [1, 2, [3, 4, [5]]])
+print(result)  # Output: [1, 4, [9, 16, [25]]]
